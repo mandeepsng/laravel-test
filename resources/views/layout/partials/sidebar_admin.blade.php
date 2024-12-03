044d2d0558be245f9b7b0bfad2c9d3cdb6a4769a
ankitathakur-developer <at9008591@gmail.com>
2024-12-04 00:18:37 +0530

    add changes

--- resources/views/layout/partials/sidebar_admin.blade.php
+++ resources/views/layout/partials/sidebar_admin.blade.php
@@ -50,19 +50,7 @@
                 <span class="ms-2">User List</span>
              </a>
           </li>
-          <li class="nav-item">
-             <a class="nav-link" href=" ">
-                <i class="align-bottom bx bx-home"></i>
-                <span class="ms-2">Role List</span>
-             </a>
-          </li>
-          <li class="nav-item">
-             <a class="nav-link" href="  ">
-                <i class="align-bottom bx bx-home"></i>
-                <span class="ms-2">Permissions</span>
-             </a>
-          </li>
-          
+         
           <li class="nav-item">
              <a class="nav-link" href="account-profile">
                 <i class="align-bottom bx bx-user"></i>
